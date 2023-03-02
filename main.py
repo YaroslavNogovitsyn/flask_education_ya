6799f4b498667bc57928851f636aaca0ac108e77
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-03-02 20:57:57 +0300

    show news on index route

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
+from data.news import News
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key'
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
 def main():
